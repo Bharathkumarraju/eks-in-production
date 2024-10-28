c8346f48b41ee847763e2ae80a9b26737041c32b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-10-29 06:09:43 +0800

    create vpc

--- EKS/vpc/locals.tf
+++ EKS/vpc/locals.tf
@@ -1,13 +1,25 @@
 locals {
-  name     = "sample-eks-prod"
-  region   = "ap-south-1"
-  app_name = "sampleapp"
+  env  = var.env
+  region = var.region
+  region_parts  = split("-", var.region)
+  region_prefix = join("", [local.region_parts[0], substr(local.region_parts[1], 0, 1), local.region_parts[2]])
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  availability_zones = slice(sort(data.aws_availability_zones.all.names), 0, var.num_of_availability_zones)
+  log_format         = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${vpc-id} $${subnet-id} $${instance-id} $${tcp-flags} $${type} $${pkt-srcaddr} $${pkt-dstaddr} $${region} $${az-id} $${sublocation-type} $${sublocation-id}"
 
   tags = {
-    Blueprint  = local.name
-    environment = "production"
+    Environment = var.env
+    Prefix      = var.prefix
   }
+
+  vpc_name     = "${var.prefix}.${var.region_prefix}.${var.env}"
+  subnets         = cidrsubnets(var.vpc_cidr, [for v in range(9) : 4]...)
+  private_subnets = slice(local.subnets, 0, 3)
+  db_subnets      = slice(local.subnets, 3, 6)
+  public_subnets  = slice(local.subnets, 6, 9)
 }
+
+
+
--- EKS/vpc/outputs.tf
+++ EKS/vpc/outputs.tf
@@ -1,10 +1,37 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+  value = module.vpc.vpc_id
+}
+
+output "azs" {
+  value = local.availability_zones
+}
+
+output "database_subnet_group_name" {
+  value = module.vpc.database_subnet_group_name
+}
+
+output "database_subnets_cidr_blocks" {
+  value = module.vpc.database_subnets_cidr_blocks
 }
 
 output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+  value = module.vpc.private_subnets
 }
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
+
+output "database_subnets" {
+  value = module.vpc.database_subnets
+}
+
 
