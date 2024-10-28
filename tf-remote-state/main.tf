e70481e95cbf187e041bc074f1b6da53305da374
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-10-28 21:26:42 +0800

    fix module path

--- tf-remote-state/main.tf
+++ tf-remote-state/main.tf
@@ -1,8 +1,8 @@
 module "state_storage" {
   source = "../modules/terraform-backend"
 
-  aws_kms_alias                       = "terraform-state-storage-bharath-mumbai"
-  tf_state_storage_bucket_name        = "terraform-state-storage-bharath-mumbai"
-  tf_state_storage_dynamodb_lock_name = "terraform-state-storage-bharath-mumbai"
+  aws_kms_alias                       = "tf-state-storage-bharath-mumbai"
+  tf_state_storage_bucket_name        = "tf-state-storage-bharath-mumbai"
+  tf_state_storage_dynamodb_lock_name = "tf-state-storage-bharath-mumbai"
   aws_account_id                      = data.aws_caller_identity.current.account_id
 }
